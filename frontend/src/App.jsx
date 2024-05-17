f1dbaa2b2c4d9963f8b248c654b28b3b84b88d9a
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-17 23:03:37 +0200

    importing the Login and Router from react-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { Toaster } from 'react-hot-toast';
+import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <Signup />
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
       <Toaster />
-    </>
+    </div>
   );
 }
 
