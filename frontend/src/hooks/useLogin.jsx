ebfe356f11b56f2c9802c6cc030f442941ceb76f
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-17 23:05:40 +0200

    login hooks that will be performing the post request to the server

---
+++ frontend/src/hooks/useLogin.jsx
@@ -0,0 +1,46 @@
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+import { useState } from 'react';
+
+const useLogin = () => {
+  const { setAuthUser } = useAuthContext();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = async ({ email, password }) => {
+    const success = handleErrors({ email, password });
+    if (!success) return;
+
+    setIsLoading(true);
+    try {
+      const res = await fetch('http://localhost:5000/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.message === 'Invalid credentials') {
+        toast.error(data.message);
+        return false;
+      }
+      localStorage.setItem('users-auth', JSON.stringify(data));
+      setAuthUser(data);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { login, isLoading };
+};
+export default useLogin;
+
+const handleErrors = ({ email, password }) => {
+  if (!email || !password) {
+    toast.error('Please fill in all fields');
+    return false;
+  }
+  return true;
+};
