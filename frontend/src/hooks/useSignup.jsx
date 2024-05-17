f1b3a24be6e6e0308881f55c013e4e273f31583f
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-17 19:57:10 +0200

    implementing the singup page and fetching the data from the backend API

---
+++ frontend/src/hooks/useSignup.jsx
@@ -0,0 +1,58 @@
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+import { useState } from 'react';
+
+const useSignup = () => {
+  const { setAuthUser } = useAuthContext();
+  const { loading, setLoading } = useState(false);
+  const signup = async ({ username, email, password, confirmPassword }) => {
+    const success = handleErrors({
+      username,
+      email,
+      password,
+      confirmPassword,
+    });
+    if (!success) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch('http://localhost:5000/api/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password, confirmPassword }),
+      });
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem('users-auth', JSON.stringify(data));
+      setAuthUser(data);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { signup, loading };
+};
+
+export default useSignup;
+
+function handleErrors({ username, email, password, confirmPassword }) {
+  if (!username || !email || !password || !confirmPassword) {
+    toast.error('Please fill in all fields');
+    return false;
+  }
+  if (password !== confirmPassword) {
+    toast.error('Passwords do not match');
+    return false;
+  }
+  if (password.length < 8) {
+    toast.error('Password must be at least 8 characters');
+    return false;
+  }
+
+  return true;
+}
---
+++ frontend/src/pages/Signup/Signup.jsx
@@ -0,0 +1,118 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import useSignup from '../../hooks/useSignup';
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const { signup, loading } = useSignup();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(formData); 
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className=" p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 ">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <span className="text-blue-500"> OnlineShopping</span>
+        </h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className=" flex flex-col items-center pt-10 "
+        >
+          <div>
+            <label htmlFor="fullName" className="label p-2 ">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="John Doe"
+              name="username"
+              value={formData.username}
+              className="input input-bordered  h-10"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email" className="label p-2 ">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="example@gmail.com"
+              name="email"
+              value={formData.email}
+              className="input input-bordered h-10"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="label">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              value={formData.password}
+              className="input input-bordered h-10"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="confirmPassword" className="label">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              id="confirmPassword"
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              className=" input input-bordered h-10"
+              onChange={handleChange}
+            />
+          </div>
+          <Link
+            to={'/login'}
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+            href="#"
+          >
+            Already have an account?
+          </Link>
+          <div>
+            <button className="btn w-60 btn-sm mt-2 border border-slate-700">
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                'Sign Up'
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
