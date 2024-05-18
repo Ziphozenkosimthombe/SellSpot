84c604d4e2e5e028e06d507f425a20facc612e20
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-19 00:49:30 +0200

    middleware handling the error from the controller

---
+++ backend/middleware/error.middleware.js
@@ -0,0 +1,9 @@
+const errorHandle = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export default errorHandle;
