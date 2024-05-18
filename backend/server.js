4c618c6ec4164b1c4087ba82844600d7851878bb
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-19 00:48:17 +0200

    importing the middleware to handle the error from the routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import morgan from 'morgan';
 import connectDB from './config/database.config';
 import userRoutes from './routes/user.routes';
 import applyRoutes from './routes/seller.routes';
+import errHandle from './middleware/error.middleware'
+import notFound from './middleware/notfound.middleware'
 dotenv.config({ path: './config/.env' });
 connectDB();
 
@@ -20,6 +22,11 @@ app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 //Routes
 app.use('/api/users', userRoutes);
 app.use('/api/apply', applyRoutes);
+
+//error handle
+app.use(notFound)
+app.use(errHandle)
+
 
 app.listen(process.env.PORT, () =>
   console.log('server is running at http://localhost:5000')
