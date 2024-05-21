6f61f6f4bd10cf270ceccff094c493727f4f4906
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-21 20:07:18 +0200

    updating cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors());
 
 //app.use(logger);
 
