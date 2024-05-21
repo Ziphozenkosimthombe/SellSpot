76af4c736c5341174941826c5990dd6d7a1671fb
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-21 13:22:20 +0200

    changing the approuch of my test cases so that it will be more readable

--- backend/test/setup.test.js
+++ backend/test/setup.test.js
@@ -1,12 +1,6 @@
 process.env.NODE_ENV = 'test';
 
-import User from '../models/User.models';
-
-
-beforeEach(async () => {
-  await User.deleteMany({});
-  
-});
+//import User from '../models/User.models';
 
 // afterEach(async () => {
 //    await User.deleteMany({})
