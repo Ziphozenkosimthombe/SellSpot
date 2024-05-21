92992b00f3d8c621f779f4e47864fa741188d959
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-21 21:32:19 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(morgan('dev'));
 app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(morgan('dev'));
 
 //app.use(logger);
 
