730e6bebde0401c4214de66de10ee07de08695bf
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-22 14:01:15 +0200

    changing changing the Link hook so that it will be reusable

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import MenuButton from './MenuButton';
 import NavigationLinks from './NavigationLinks';
 import DarkMode from './DarkAndLight';
-import { Link } from 'react-router-dom';
+import Links from './Links';
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -18,52 +18,20 @@ const Header = () => {
     <div>
       <nav className="fixed w-screen" style={navHeight}>
         <div className="container mx-auto px-4 py-2 flex justify-between items-center relative mt-5">
-          {/* Menu button for small screens */}
           <MenuButton menuOpen={menuOpen} toggleMenu={toggleMenu} />
-          {/* Menu rendered inside the nav tag for larger screens */}
-          <Link to={'/'} className=" text-xl font-bold">
-            SellSpot
-          </Link>
+          <div className="text-2xl font-bold">
+            <Links to={'/'} text={'SellSpot'} />
+          </div>
           <NavigationLinks />
-          {/* Add the DarkMode component here */}
         </div>
       </nav>
-      {/* Sidebar rendered for small screens */}
+
       {menuOpen && (
         <div className="md:hidden shadow-2xl  w-48 fixed top-16 left-0 h-full pt-16">
           <ul className="">
-            <li>
-              <Link
-                to={'/login'}
-                className="block px-4 py-2 hover:text-gray-400"
-              >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={'/signup'}
-                className="block px-4 py-2 hover:text-gray-400"
-              >
-                Register
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={'/apply'}
-                className="block px-4 py-2 hover:text-gray-400"
-              >
-                Sell on SellSpot
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={'/aboutUs'}
-                className="block px-4 py-2 hover:text-gray-400"
-              >
-                About us
-              </Link>
-            </li>
+            <Links to={'/login'} text={'Login'} />
+            <Links to={'/signup'} text={'Register'} />
+            <Links to={'/aboutUs'} text={'About Us'} />
             <li className="py-2 px-4">
               <DarkMode />
             </li>
