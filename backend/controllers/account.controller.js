463bd5cd56e06f7597951775dbda1003303dad2f
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-25 01:08:09 +0200

    fixxing with eslint

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/User.models';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 class AccountController {
   static async getAccount(req, res, next) {
     try {
@@ -26,27 +26,31 @@ class AccountController {
         error.status = 404;
         return next(error);
       }
-        const isPasswordMatch = await bcrypt.compare(currentPassword, user.password);
-        if (!isPasswordMatch) {
-            const error = new Error('Wrong password');
-            error.status = 400
-            return next(error)
-        }
+      const isPasswordMatch = await bcrypt.compare(
+        currentPassword,
+        user.password
+      );
+      if (!isPasswordMatch) {
+        const error = new Error('Wrong password');
+        error.status = 400;
+        return next(error);
+      }
       user.username = username || user.username;
       user.phoneNumber = phoneNumber || user.phoneNumber;
-      
-        if(newPassword){
-            const salt = await bcrypt.genSalt(10)
-            user.password = await bcrypt.hash(newPassword, salt)
-        }
+
+      if (newPassword) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(newPassword, salt);
+      }
       await user.save();
-      return res.status(200).json({message: 'Account successful updated', user});
+      return res
+        .status(200)
+        .json({ message: 'Account successful updated', user });
     } catch (err) {
       console.log('Error on the account.controller updateAccount', err);
       return next(err);
     }
   }
-
 }
 
 export default AccountController;
