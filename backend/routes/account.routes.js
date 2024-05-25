405c3f17927f034b39db24a55579bf5f98da0ec7
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-25 15:05:47 +0200

    adding the delete http method

--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.get('/account', protectRoute, AccountController.getAccount);
 router.put('/account', protectRoute, AccountController.updateAccount);
+router.delete('/account', protectRoute, AccountController.deleteAccount);
 export default router;
