0587bcd9ec0386d6546d77c076d1232b30aa7170
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-25 15:05:05 +0200

    allowing the user to be able to delete the account and also it will delete the images of the products in the cloudinary if the user is selling on the platform

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,4 +1,9 @@
 import User from '../models/User.models';
+import cloudinary from '../middleware/cloudinary.middleware';
+import Seller from '../models/Seller.model';
+import Carts from '../models/Carts.models';
+import List from '../models/List.models';
+import Products from '../models/Products.models';
 import bcrypt from 'bcryptjs';
 class AccountController {
   static async getAccount(req, res, next) {
@@ -48,6 +53,48 @@ class AccountController {
         .json({ message: 'Account successful updated', user });
     } catch (err) {
       console.log('Error on the account.controller updateAccount', err);
+      return next(err);
+    }
+  }
+  static async deleteAccount(req, res, next) {
+    try {
+      const userId = req.user._id;
+
+      // Delete user account
+      const user = await User.findByIdAndDelete(userId);
+      if (!user) {
+        const error = new Error('User not found');
+        error.status = 404;
+        return next(error);
+      }
+
+      // Delete seller account if exists
+      const seller = await Seller.findOneAndDelete({ user: userId });
+
+      // Delete cart associated with the user
+      await Carts.findOneAndDelete({ user: userId });
+
+      // Delete wishlist associated with the user
+      await List.findOneAndDelete({ user: userId });
+
+      // Fetch products sold by the user
+      const products = await Products.find({
+        seller: seller ? seller._id : userId,
+      });
+
+      // Delete images from Cloudinary
+      for (const product of products) {
+        for (const imageId of product.cloudinaryId) {
+          await cloudinary.uploader.destroy(imageId);
+        }
+      }
+
+      // Delete products sold by the user
+      await Products.deleteMany({ seller: seller ? seller._id : userId });
+
+      return res.status(200).json({ message: 'Account successfully deleted' });
+    } catch (err) {
+      console.log('Error on the account.controller deleteAccount', err);
       return next(err);
     }
   }
