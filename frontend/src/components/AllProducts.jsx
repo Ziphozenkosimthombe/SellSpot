b4e2acfa0c27ca38c8ce83eb14aade93389d2b51
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-05-28 12:04:47 +0200

    adding the styling on my products

--- frontend/src/components/AllProducts.jsx
+++ frontend/src/components/AllProducts.jsx
@@ -9,21 +9,21 @@ const AllProductList = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((product) => (
           <Link to={`/products/${product._id}`} key={product._id}>
-            <div className="col-span-1 p-4 border rounded-lg shadow cursor-pointer">
+            <div className="col-span-1 p-4  rounded-2xl shadow-2xl cursor-pointer all-product">
               <div className="aspect-w-1 aspect-h-1">
                 {product.images.length > 0 && (
                   <img
                     src={product.images[0]}
                     alt={`${product.title} 1`}
-                    className="object-cover"
+                    className="object-cover image rounded-l-xl pl-2.5"
                   />
                 )}
               </div>
-              <h2 className="text-xl font-bold">{product.title}</h2>
-              <p className="text-lg font-bold">
+              <h2 className="text-xl font-bold pl-2.5 pt-3">{product.title}</h2>
+              <p className="text-2xl font-bold text-green-600 mb-1 pl-2.5">
                 R{product.price.toLocaleString()}
               </p>
-              <span>({product.stock_quantity})</span>
+              <span className="pl-2.5 ">({product.stock_quantity})</span>
             </div>
           </Link>
         ))}
